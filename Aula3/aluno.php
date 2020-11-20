6482878c68a6becbd6b6d6bff4a886399a0196e1
Rafael Silva <rafsilva19@gmail.com>
2020-11-20 10:38:10 +0000

    Aula3 (final)

--- Aula3/aluno.php
+++ Aula3/aluno.php
@@ -2,9 +2,13 @@
 <html>
     <head>
         <title>Aluno</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
         <link rel="stylesheet" href="css/estilos.css" />        
     </head>
     <body>
+
+        <?php include "includes/menu.php"; ?>
 
         <div class="alunoDetalhe">
 
@@ -16,8 +20,6 @@
             <p><b>Turma:</b> Informática</p>
             <p><b>E-mail:</b> <a href="mailto:teste@teste.pt">teste@teste.pt</a></p>
             <p><b>Telemóvel:</b> +351 918 562 523</p>
-
-            
 
             <hr />
 
@@ -71,5 +73,8 @@
 
         </div>
 
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     </body>
 </html>
--- Aula3/turma.php
+++ Aula3/turma.php
@@ -2,24 +2,35 @@
 <html lang="en">
     <head>
         <title>Turma</title>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
         <link rel="stylesheet" href="css/estilos.css" />
     </head>
     <body>
 
+    <?php include "includes/menu.php"; ?>
+
     <?php 
-    $alunos = array('João', 'Pedro', 'Maria', 'João', 'Pedro', 'Maria', 'João', 'Pedro', 'Maria', 'João', 'Pedro', 'Maria', 'Ana');
+    $alunos = array(
+        '3' => 'Bruno',
+        '7' => 'Azedo',
+        '15' => 'Lucia',
+    );
     ?>
 
         <?php foreach ($alunos as $key => $value) { ?>
             
             <div class="aluno">
                 <div class="alunoContent">
-                    <img src="img/1.png" alt="<?php echo $value; ?>" />
-                    <a href="aluno.php?nome=<?php echo $value; ?>"><?php echo $value; ?></a>
+                    <img src="img/<?php echo $key; ?>.jpg" alt="<?php echo $value; ?>" />
+                    <a href="aluno.php?nome=<?php echo $value; ?>"><?php echo $key . " - " . $value; ?></a>
                 </div>
             </div>
 
         <?php } ?>
-
+        
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     </body>
 </html>
